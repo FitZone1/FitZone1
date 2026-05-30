62c21e668a20eabed9d31ac9de2c9c6dfd589725
spotifyconnor3-create <spotify.connor3@gmail.com>
2026-05-30 11:40:28 -0500

    mainjohan2

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ app.include_router(reserva_sesion_router)
 app.include_router(cancelar_reserva_router)
 app.include_router(gestion_horarios_router)
 app.include_router(control_permisos_router)
+app.include_router(pago_mensualidad_router)
+app.include_router(procesar_planes_router)
 # Ruta raíz — bienvenida
 @app.get("/", tags=["Root"])
 def root():
