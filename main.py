8801cbb2cb9fba6aeed67cbe8342825fa4345d54
spotifyconnor3-create <spotify.connor3@gmail.com>
2026-06-06 09:18:29 -0500

    main

--- main.py
+++ main.py
@@ -11,20 +11,14 @@ from app.api.Cliente.ReservaSesion_api import router as reserva_sesion_router
 from app.api.Cliente.CancelarReserva_api import router as cancelar_reserva_router
 from app.api.Entrenador.GestionHorarios_api import router as gestion_horarios_router
 from app.api.ControlPermisos.ControlPermisos_api import router as control_permisos_router
-<<<<<<< HEAD
 from app.api.Cliente.PagoMensualidad_api import router as pago_mensualidad_router
 from app.api.Cliente.procesarplanes_api import router as procesar_planes_router
-=======
 from app.api.Cliente.PagoMensualidad_api import router as pagos_mesualidad_router
 from app.api.Cliente.PagoMensualidad_api import router as pagar_mensualidad_router
 from app.api.ControlPermisos.ControlPermisos_api import router as control_permisos_router
 from app.api.ControlPermisos.ControlPermisos_api import router as control_permisos_router   
 from app.api.Cliente.PagoMensualidad_api import router as pago_mensualidad_router
 from app.api.Cliente.procesarplanes_api import router as procesar_planes_router
-
-
->>>>>>> 15215084167193e2b755cc9b61b21caeed2c2961
-
 # Crear la aplicación con metadata para la documentación
 app = FastAPI(
     title="FitZone API",
@@ -41,16 +35,12 @@ app.include_router(reserva_sesion_router)
 app.include_router(cancelar_reserva_router)
 app.include_router(gestion_horarios_router)
 app.include_router(control_permisos_router)
-<<<<<<< HEAD
 app.include_router(pago_mensualidad_router)
 app.include_router(procesar_planes_router)
-=======
 app.include_router(pagos_mesualidad_router)
 app.include_router(pagar_mensualidad_router)
 app.include_router(pago_mensualidad_router)
 app.include_router(procesar_planes_router)
-
->>>>>>> 15215084167193e2b755cc9b61b21caeed2c2961
 # Ruta raíz — bienvenida
 @app.get("/", tags=["Root"])
 def root():
