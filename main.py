6b6bd595ebe28a6628e81e2b277b4bf37483e535
angelasviviescas10-code <angelasviviescas10@gmail.com>
2026-06-06 11:24:41 -0500

    main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from app.api.ControlPermisos.ControlPermisos_api import router as control_permis
 from app.api.Cliente.PagoMensualidad_api import router as pagos_mesualidad_router
 from app.api.ControlPermisos.ControlPermisos_api import router as control_permisos_router  
 from app.api.Cliente.procesarplanes_api import router as procesar_planes_router
-from app.domain.Pagos.ProcesarFactura_domain import procesar_factura_router
+from app.domain.Pagos.ProcesarFactura_domain import procesar_facturas_router
 
 
 # Crear la aplicación con metadata para la documentación
@@ -35,7 +35,7 @@ app.include_router(gestion_horarios_router)
 app.include_router(control_permisos_router)
 app.include_router(pagos_mesualidad_router)
 app.include_router(procesar_planes_router)
-app.include_router(procesar_factura_router)
+app.include_router(procesar_facturas_router)
 
 
 # Ruta raíz — bienvenida
